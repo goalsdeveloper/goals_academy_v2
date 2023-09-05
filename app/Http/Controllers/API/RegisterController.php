27ae8a5e1aa14cfc73253a488fbbaf25f5fd77d7
Ekadian Haris <ekadianharis@gmail.com>
2023-09-05 19:48:34 +0700

    add Auth route to API, also install Laravel Passport

---
+++ app/Http/Controllers/API/RegisterController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $rule = [
+            'username' => 'required|max:15',
+            'email' => 'required|email:dns',
+            'password' => 'required',
+            're-password' => 'required'
+        ];
+
+        $validateData = $request->validate($rule);
+        if (!$validateData) {
+            return response()->json(['message' => 'Invalid Data']);
+        }
+
+        // if the account is already exist
+        $user = User::where('email', $validateData['email'])->first();
+        if ($user) {
+            abort(409, 'Account already exist!');
+        }
+
+        $userByUsername = User::where('username', $validateData['username'])->first();
+        if ($userByUsername) {
+            abort(409, 'Username already exist!');
+        }
+
+        if ($validateData['password'] !== $validateData['re-password']) {
+            abort(409, 'Your password is not match!');
+        }
+
+        User::create($validateData);
+
+        return response()->json([
+            'message' => 'Your account is created successfully! Please login.',
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
