e47d2b25d9d5abfb620c5a63f5e5b2bbf88945c9
Ekadian Haris <ekadianharis@gmail.com>
2023-09-27 19:21:42 +0700

    login using socialite success

---
+++ app/Models/Social.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Social extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'provider_id', 'provider_name',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
