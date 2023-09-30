7b4a76eb742b71dfd92403772a843fe7c1a584b9
Akhmad Roziqin <roziqinakhmad14juli@gmail.com>
2023-09-30 09:34:49 +0700

    Update PorfilTutor UI/UX

--- resources/js/Components/CornerWaveVector.jsx
+++ resources/js/Components/CornerWaveVector.jsx
@@ -1,13 +1,17 @@
 import wave1 from "/resources/img/vector/wave-1.svg";
 import wave2 from "/resources/img/vector/wave-2.svg";
 
-export default function CornerWaveVector ({ className, cornerClassName, rightCornerClassName, leftCornerClassName }) {
+export default function CornerWaveVector ({ className, cornerClassName, rightCornerClassName, leftCornerClassName, corner2ClassName='hidden', right2CornerClassName, left2CornerClassName }) {
     return (
         <div className={`select-none ${className}`}>
             <img className={`absolute bottom-0 left-0 ${cornerClassName} ${rightCornerClassName}`} src={wave1} />
             <img className={`absolute bottom-0 left-0 ${cornerClassName} ${rightCornerClassName}`} src={wave2} />
             <img className={`absolute top-0 right-0 -scale-x-1 -scale-y-1 ${cornerClassName} ${leftCornerClassName}`} src={wave1} />
             <img className={`absolute top-0 right-0 -scale-x-1 -scale-y-1 ${cornerClassName} ${leftCornerClassName}`} src={wave2} />
+            <img className={`absolute bottom-0 right-0 -scale-x-1 ${corner2ClassName} ${right2CornerClassName}`} src={wave1} />
+            <img className={`absolute bottom-0 right-0 -scale-x-1 ${corner2ClassName} ${right2CornerClassName}`} src={wave2} />
+            <img className={`absolute top-0 left-0 -scale-y-1 ${corner2ClassName} ${left2CornerClassName}`} src={wave1} />
+            <img className={`absolute top-0 left-0 -scale-y-1 ${corner2ClassName} ${left2CornerClassName}`} src={wave2} />
         </div>
     )
 }
--- resources/js/Pages/Partials/ProfilTutor/Hero.jsx
+++ resources/js/Pages/Partials/ProfilTutor/Hero.jsx
@@ -3,15 +3,14 @@ import figure6 from '/resources/img/figure/6.svg';
 export default function Hero () {
     return (
         <section id="hero" className="my-14 xs:my-16 xl:my-24 3xl:my-32">
-            <div className="container mx-auto relative flex flex-col md:flex-row justify-between items-center md:pt-4 lg:pt-8 xl:pt-12 3xl:py-20">
-                <div className="w-full md:w-7/12 md:absolute h-full flex flex-col justify-center text-center md:text-start mb-8 md:mb-0">
+            <div className="container mx-auto relative flex flex-col justify-between items-center md:pt-4 lg:pt-8 xl:pt-8 3xl:py-20">
+                <div className="w-fullh-full flex flex-col justify-center text-center mb-8 md:mb-0">
                     {/* <div className="md:before:bg-tree-1 before:absolute before:-z-10 before:bg-no-repeat md:before:w-32 md:before:h-32 lg:before:w-40 lg:before:h-40 xl:before:w-56 xl:before:h-56 2xl:before:w-64 2xl:before:h-64 3xl:before:w-80 3xl:before:h-80 md:before:-translate-x-3/4 3xl:before:-translate-x-[80%] md:before:-translate-y-8 lg:before:-translate-y-10 xl:before:-translate-y-12 3xl:before:-translate-y-20">
                     </div> */}
-                    <h2>Para Tutor yang <br className="md:hidden" /><span className="text-primary">Berkeahlian di Bidangnya</span></h2>
+                    <h2>Para Tutor yang <br /><span className="text-primary">Berkeahlian di Bidangnya</span></h2>
                     {/* <p className="md:mt-2 xl:mt-4">Membangun Pemimpin Masa Depan melalui Inovasi dan Teknologi.</p> */}
                 </div>
-                <div></div>
-                <div className="w-full md:w-8/12 md:translate-x-[20%] 3xl:translate-x-[30%] -z-10">
+                <div className="w-full">
                     <img className="w-full md:w-11/12 float-right" src={figure6} alt="" />
                 </div>
             </div>
