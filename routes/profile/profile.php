5e3ddecfa4366d877e937c14dffe951d0460b4bf
Ekadian Haris <ekadianharis@gmail.com>
2023-11-21 17:57:14 +0700

    halaman profile 40% proggress

--- routes/profile/profile.php
+++ routes/profile/profile.php
@@ -1,15 +1,12 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function () {
-    return Inertia::render('Auth/User/Index');
-});
+Route::get('/user', [ProfileController::class, 'index'])->name('user.profile');
 
-Route::get('/pembelajaran_saya', function () {
-    return Inertia::render('Auth/User/PembelajaranSaya');
-});
+Route::get('/pembelajaran_saya', [ProfileController::class, 'pembelajaranSaya'])->name('user.profile.pembelajaranSaya');
 
 Route::get('/riwayat_transaksi', function () {
     return Inertia::render('Auth/User/RiwayatTransaksi');
