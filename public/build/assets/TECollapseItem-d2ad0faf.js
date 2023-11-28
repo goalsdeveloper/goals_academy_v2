c5423ade1a8854e6a4d6550779bb8fed815dc97a
Ekadian Haris <ekadianharis@gmail.com>
2023-11-28 19:20:54 +0700

    run build for test

--- public/build/assets/TECollapseItem-d2ad0faf.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./app-c2aaa1c0.js";function n({className:r,children:t}){return s.jsxs(s.Fragment,{children:[s.jsx("br",{}),s.jsx("div",{className:`grid ${r}`,children:t})]})}export{n as T};
