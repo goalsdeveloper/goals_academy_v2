298b0c87aab17d202d0f568a46d9a0fe622e9669
afanokta <afanoktafianto99@gmail.com>
2024-02-19 17:51:05 +0700

    add code to drop column category_id

--- database/migrations/2024_02_19_174718_add_fk_category_to_products.php
+++ database/migrations/2024_02_19_174718_add_fk_category_to_products.php
@@ -24,6 +24,7 @@ return new class extends Migration
     {
         Schema::table('products', function (Blueprint $table) {
             $table->dropForeign('products_category_id_foreign');
+            $table->dropColumn('category_id');
         });
     }
 };
