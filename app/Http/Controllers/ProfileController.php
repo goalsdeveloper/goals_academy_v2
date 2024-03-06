f882e42074a67174494b9a2802efc2dad42525cf
afanokta <afanoktafianto99@gmail.com>
2024-03-06 15:27:16 +0700

    centralized profile controller for use dashboard

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Enums\OrderEnum;
+use App\Models\Course;
 use App\Models\Order;
 use App\Models\User;
+use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Support\Benchmark;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
@@ -38,11 +41,60 @@ class ProfileController extends Controller
             ->whereHas('products.category', function ($query) {
                 $query->where('name', 'like', '%dibimbing%');
             })
-            ->with('products.category', 'course')
+            ->with('products.category', 'course', 'products')
             ->get();
-
         return Inertia::render('Auth/User/Bimbingan/Bimbingan', [
             'orderBimbingan' => $orderBimbingan,
+        ]);
+    }
+
+    public function webinar()
+    {
+        $user = Auth::user();
+        $orderWebinar = Order::where('user_id', $user->id)
+            ->where('status', OrderEnum::SUCCESS->value)
+            ->whereHas('products.productType', function ($query) {
+                $query->where('type', 'like', '%webinar%');
+            })
+            ->with('products.category', 'course')
+            ->get();
+        return Inertia::render('Auth/User/Webinar/Webinar', [
+            'orderWebinar' => $orderWebinar,
+        ]);
+    }
+
+    public function detailWebinar()
+    {
+        $user = Auth::user();
+        $orderWebinar = Order::where('user_id', $user->id)
+            ->where('status', OrderEnum::SUCCESS->value)
+            ->whereHas('products.category', function ($query) {
+                $query->where('name', 'like', '%webinar%');
+            })
+            ->with('products.category', 'course')
+            ->get();
+        if ($orderWebinar->isEmpty()) {
+            return abort(404);
+        }
+        return Inertia::render('Auth/User/Webinar/DetailWebinar', ['orderWebinar' => $orderWebinar]);
+    }
+
+    public function detailPembelajaran(string $id)
+    {
+        $user = Auth::user();
+        $course = Course::whereHas('order', function (Builder $query) use ($id, $user) {
+            $query->where('order_code', $id)->where('user_id', $user->id);
+        })->whereHas('products.productType', function (Builder $query) {
+            $query->where('type', 'like', '%bimbingan%');
+        })
+            ->with('order', 'tutor', 'tutorNote', 'fileUploads', 'products')
+            ->get();
+        if ($course->isEmpty()) {
+            return abort(404);
+        }
+        dd($course);
+        return Inertia::render('Auth/User/Bimbingan/DetailBimbingan', [
+            'courseDetail' => $course,
         ]);
     }
 }
