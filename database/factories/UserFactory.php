f758fcadb4e2c2975ffd6c40c2c26b6105322f80
afanokta <afanoktafianto99@gmail.com>
2024-03-14 12:57:45 +0700

    add user seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,6 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'username' => fake()->userName(),
             'email' => fake()->unique()->safeEmail(),
-            'user_role' => 'user',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => null,
@@ -35,8 +34,15 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    public function role($role)
+    {
+        return $this->state(fn(array $attributes) => [
+            'user_role' => $role,
         ]);
     }
 }
