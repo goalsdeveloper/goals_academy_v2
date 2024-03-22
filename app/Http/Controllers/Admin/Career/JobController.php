4052d86f0e40aa0278a6432519473c44fe856d14
ilhamarkan2004 <ilhamarkan2004@gmail.com>
2024-03-22 16:14:46 +0700

    add controller

---
+++ app/Http/Controllers/Admin/Career/JobController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Career;
+
+use App\Http\Controllers\Controller;
+use App\Models\Job;
+use Illuminate\Http\Request;
+
+class JobController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $jobs = Job::with('division', 'location')->paginate(10);
+        return response()->json(['status' => true, 'statusCode' => 200, 'message' => 'get data job success', 'data' => $jobs], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- app/Http/Controllers/Admin/Career/ParticipantController.php
+++ app/Http/Controllers/Admin/Career/ParticipantController.php
@@ -1,20 +1,18 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers\Admin\Career;
 
 use App\Http\Controllers\Controller;
-use App\Models\Job;
 use Illuminate\Http\Request;
 
-class JobController extends Controller
+class ParticipantController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        $jobs = Job::with('division', 'location')->paginate(10);
-        return response()->json(['status' => true, 'statusCode' => 200, 'message' => 'get data job success', 'data' => $jobs], 200);
+        //
     }
 
     /**
