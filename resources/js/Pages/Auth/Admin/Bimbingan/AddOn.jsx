2e14f58c1518d0fc87d2469e3bf0b3d62a51785c
Naufal Romero <88795011+romves@users.noreply.github.com>
2024-03-28 16:45:43 +0700

    feat(admin-dash): add table layout

--- resources/js/Pages/Auth/Admin/Bimbingan/AddOn.jsx
+++ resources/js/Pages/Auth/Admin/Bimbingan/AddOn.jsx
@@ -1,9 +1,91 @@
 import DashboardLayout from "@/Layouts/DashboardLayout";
+import { SubHeading } from "./Product";
+import { FiEdit2, FiPlus, FiTrash2 } from "react-icons/fi";
+import GoalsDashboardTable from "@/Components/elements/GoalsDashboardTable";
+import { Link } from "@inertiajs/react";
+import { useMemo } from "react";
 
-export default function AddOn ({ auth }) {
+export default function AddOn({ auth }) {
+    const columns = useMemo(
+        () => [
+            {
+                accessorKey: "name",
+                header: "Nama",
+                size: 200,
+            },
+            {
+                accessorKey: "slug",
+                header: "Slug",
+                size: 200,
+            },
+            {
+                accessorKey: "harga",
+                header: "Harga",
+                size: 200,
+            },
+            {
+                accessorKey: "action",
+                header: "Action",
+                size: 50,
+
+                Cell: ({ cell }) => (
+                    <ul className="flex gap-[.8vw] w-fit">
+                        <li>
+                            <Link href="/admin/bimbingan/product/edit">
+                                <FiEdit2 className="text-[1.2vw] text-secondary" />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                method="DELETE"
+                                href={`/admin/bimbingan/product/${cell.getValue()}`}
+                            >
+                                <FiTrash2 className="text-[1.2vw] text-danger-40" />
+                            </Link>
+                        </li>
+                    </ul>
+                ),
+            },
+        ],
+        []
+    );
+
     return (
-        <DashboardLayout title="Bimbingan" subtitle="Add-On" role="admin" auth={auth}>
-            <div>Add-On</div>
+        <DashboardLayout
+            title="Bimbingan"
+            subtitle="Add-On"
+            role="admin"
+            auth={auth}
+        >
+            <div className="space-y-[1.6vw]">
+                <SubHeading title="Add-On">
+                    <Link
+                        isLink
+                        href="/admin/bimbingan/product/add"
+                        className="flex items-center gap-[.5vw] bg-secondary hover:bg-primary text-white py-[.6vw] px-[1.2vw] rounded-[.4vw] text-[.7vw]"
+                    >
+                        <FiPlus className="text-[1vw]" />
+                        Add-On
+                    </Link>
+                </SubHeading>
+
+                <GoalsDashboardTable
+                    isHeadVisible
+                    isPaginated
+                    isSortable
+                    columns={columns}
+                    data={data}
+                />
+            </div>
         </DashboardLayout>
-    )
+    );
 }
+
+const data = [
+    {
+        id: 1,
+        name: "Record",
+        slug: "record",
+        harga: 15000,
+    },
+];
--- resources/js/Pages/Auth/Admin/Bimbingan/Order.jsx
+++ resources/js/Pages/Auth/Admin/Bimbingan/Order.jsx
@@ -1,9 +1,81 @@
 import DashboardLayout from "@/Layouts/DashboardLayout";
+import { SubHeading } from "./Product";
+import { Link } from "@inertiajs/react";
+import { FiPlus } from "react-icons/fi";
+import GoalsDashboardTable from "@/Components/elements/GoalsDashboardTable";
+import { useMemo } from "react";
 
-export default function Order ({ auth }) {
+export default function Order({ auth }) {
+    const columns = useMemo(
+        () => [
+            {
+                accessorKey: "order_id", //access nested data with dot notation
+                header: "Id Pesanan",
+
+                Cell: ({ cell }) => {
+                    return (
+                        <img
+                            src={cell.row.original.gambar}
+                            alt="thumbnail-product"
+                            className="w-[3.6vw] h-[2.6vw] rounded-[.3vw]"
+                        />
+                    );
+                },
+            },
+            {
+                accessorKey: "name",
+                header: "Nama Pembeli",
+            },
+            {
+                accessorKey: "product",
+                header: "Produk",
+            },
+            {
+                accessorKey: "payment_method",
+                header: "Pembayaran",
+            },
+            {
+                accessorKey: "status",
+                header: "Status",
+            },
+            {
+                accessorKey: "order_date",
+                header: "Tanggal Pesanan",
+            },
+        ],
+        []
+    );
+
     return (
-        <DashboardLayout title="Bimbingan" subtitle="Order" role="admin" auth={auth}>
-            <div>Order</div>
+        <DashboardLayout
+            title="Bimbingan"
+            subtitle="Order"
+            role="admin"
+            auth={auth}
+        >
+            <div className="space-y-[1.6vw]">
+                <SubHeading title="Order" />
+
+                <GoalsDashboardTable
+                    isHeadVisible
+                    isPaginated
+                    isSortable
+                    columns={columns}
+                    data={data}
+                />
+            </div>
         </DashboardLayout>
-    )
+    );
 }
+
+const data = [
+    {
+        id: 1,
+        order_id: "12345",
+        name: "John Doe",
+        product: "Product A",
+        payment_method: "Credit Card",
+        status: "Pending",
+        order_date: "2022-01-01",
+    },
+];
