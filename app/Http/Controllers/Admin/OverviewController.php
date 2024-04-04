d415bc827fa53de59e8fdf9c56d834245d77fcc6
ilhamarkan2004 <ilhamarkan2004@gmail.com>
2024-04-04 21:38:33 +0700

    tambah views and click dan merubah data2 dioverview berdasarkan range date

--- app/Http/Controllers/Admin/OverviewController.php
+++ app/Http/Controllers/Admin/OverviewController.php
@@ -5,9 +5,13 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
 use App\Models\Products;
+use DateTime;
+use Exception;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Auth;
+use Spatie\Analytics\Facades\Analytics;
+use Spatie\Analytics\Period;
 
 class OverviewController extends Controller
 {
@@ -18,8 +22,9 @@ class OverviewController extends Controller
     {
         try {
             if (Auth::user()->user_role == "admin") {
-                $date = $request->input('date');
-                if (!$date) {
+                $startDateString = $request->input('startDate');
+                $endDateString = $request->input('endDate');
+                if (!$startDateString && !$endDateString) {
                     $order =  Order::orderBy('created_at', 'desc')->with(['user:id,username,name', 'products:id,product_type_id,category_id,name', 'products.category:id,name', 'products.productType:id,type'])->get();
                     $topSellingProducts = Products::select('products.id', 'products.name', \DB::raw('COUNT(CASE WHEN orders.status = "Success" THEN orders.id END) as order_count'))
                         ->leftJoin('orders', 'products.id', '=', 'orders.products_id')
@@ -31,37 +36,171 @@ class OverviewController extends Controller
                     $totalOrder = Order::where('status', '=', 'Success')->count();
                     $totalCheckout = Order::count();
                     $totalEarning = (int)Order::where('status', '=', 'Success')->sum('unit_price');
-                    return Inertia::render('Auth/Admin/Overview/Overview', [
+
+
+                    $limit = 1000;
+                    $period = Period::months(1);
+                    $visitorsAndPageViews = Analytics::fetchVisitorsAndPageViewsByDate($period, $limit);
+
+
+                    $totalClicks = 0;
+                    foreach ($visitorsAndPageViews as $visitorData) {
+                        $screenPageViews = $visitorData['screenPageViews'];
+                        $totalClicks += $screenPageViews;
+                    }
+
+
+                    $totalViews = 0;
+                    foreach ($visitorsAndPageViews as $visitorData) {
+                        $pageTitle = $visitorData['pageTitle'];
+                        $screenPageViews = $visitorData['screenPageViews'];
+
+
+                        if (stripos($pageTitle, 'Goals Academy') !== false) {
+                            $totalViews += $screenPageViews;
+                        }
+                    }
+
+
+                    // Inisialisasi array untuk menyimpan total klik dan total tampilan per tanggal
+                    $totalsByDate = [];
+
+
+                    foreach ($visitorsAndPageViews as $visitorData) {
+                        $date = $visitorData['date']->format('Y-m-d');
+
+
+                        if (!isset($totalsByDate[$date])) {
+                            $totalsByDate[$date] = [
+                                'totalClicks' => 0,
+                                'totalViews' => 0,
+                            ];
+                        }
+
+                        // Hitung total click dan total view untuk setiap data
+                        $screenPageViews = $visitorData['screenPageViews'];
+                        $totalsByDate[$date]['totalClicks'] += $screenPageViews;
+
+                        $pageTitle = $visitorData['pageTitle'];
+                        if (stripos($pageTitle, 'Goals Academy') !== false) {
+                            $totalsByDate[$date]['totalViews'] += $screenPageViews;
+                        }
+                    }
+
+
+
+                    return response()->json([
                         'status' => true,
                         'statusCode' => 200,
-                        'message' => 'get data category success',
                         'total_earning' => $totalEarning,
                         'total_order' => $totalOrder,
                         'total_checkout' => $totalCheckout,
                         'list_orders' => $order,
-                        "top_selling" => $topSellingProducts
+                        "top_selling" => $topSellingProducts,
+                        'totalClicksKeseluruhan' => $totalClicks,
+                        'totalViewsKeseluruhan' => $totalViews,
+                        'totalsByDate' => $totalsByDate,
                     ], 200);
                 } else {
-                    $order =  Order::whereDate('created_at', $date)->orderBy('created_at', 'desc')->with(['user:id,username,name', 'products:id,product_type_id,category_id,name', 'products.category:id,name', 'products.productType:id,type'])->get();
+                    $order = Order::query()
+                        ->whereBetween('created_at', [$startDateString, $endDateString])
+                        ->orderBy('created_at', 'desc')
+                        ->with(['user:id,username,name', 'products:id,product_type_id,category_id,name', 'products.category:id,name', 'products.productType:id,type'])
+                        ->get();
+
                     $topSellingProducts = Products::select('products.id', 'products.name', \DB::raw('COUNT(CASE WHEN orders.status = "Success" THEN orders.id END) as order_count'))
                         ->leftJoin('orders', 'products.id', '=', 'orders.products_id')
-                        ->whereDate('orders.created_at', $date)
+                        ->whereBetween('orders.created_at', [$startDateString, $endDateString])
                         ->groupBy('products.id', 'products.name')
                         ->orderByDesc('order_count')
                         ->take(5)
                         ->get();
-                    $totalOrder = Order::whereDate('created_at', $date)->where('status', '=', 'Success')->count();
-                    $totalCheckout = Order::whereDate('created_at', $date)->count();
-                    $totalEarning = (int)Order::whereDate('created_at', $date)->where('status', '=', 'Success')->sum('unit_price');
-                    return Inertia::render('Auth/Admin/Overview/Overview', [
+
+                    $totalOrder = Order::whereBetween('created_at', [$startDateString, $endDateString])
+                        ->where('status', '=', 'Success')
+                        ->count();
+
+                    $totalCheckout = Order::whereBetween('created_at', [$startDateString, $endDateString])
+                        ->count();
+
+                    $totalEarning = (int)Order::whereBetween('created_at', [$startDateString, $endDateString])
+                        ->where('status', '=', 'Success')
+                        ->sum('unit_price');
+
+
+
+
+                    $limit = 1000;
+                    try {
+                        $startDate = DateTime::createFromFormat('Y-m-d', $startDateString);
+                        $endDate = DateTime::createFromFormat('Y-m-d', $endDateString);
+
+                        if (!($startDate instanceof \DateTimeInterface) || !($endDate instanceof \DateTimeInterface)) {
+                            throw new Exception('Invalid date format. Please use YYYY-MM-DD');
+                        }
+
+                        $period = Period::create($startDate, $endDate);
+                        $visitorsAndPageViews = Analytics::fetchVisitorsAndPageViewsByDate($period, $limit);
+
+
+                        $totalClicks = 0;
+                        foreach ($visitorsAndPageViews as $visitorData) {
+                            $screenPageViews = $visitorData['screenPageViews'];
+                            $totalClicks += $screenPageViews;
+                        }
+
+
+                        $totalViews = 0;
+                        foreach ($visitorsAndPageViews as $visitorData) {
+                            $pageTitle = $visitorData['pageTitle'];
+                            $screenPageViews = $visitorData['screenPageViews'];
+
+
+                            if (stripos($pageTitle, 'Goals Academy') !== false) {
+                                $totalViews += $screenPageViews;
+                            }
+                        }
+
+
+                        // Inisialisasi array untuk menyimpan total klik dan total tampilan per tanggal
+                        $totalsByDate = [];
+
+
+                        foreach ($visitorsAndPageViews as $visitorData) {
+                            $date = $visitorData['date']->format('Y-m-d');
+
+
+                            if (!isset($totalsByDate[$date])) {
+                                $totalsByDate[$date] = [
+                                    'totalClicks' => 0,
+                                    'totalViews' => 0,
+                                ];
+                            }
+
+                            // Hitung total click dan total view untuk setiap data
+                            $screenPageViews = $visitorData['screenPageViews'];
+                            $totalsByDate[$date]['totalClicks'] += $screenPageViews;
+
+                            $pageTitle = $visitorData['pageTitle'];
+                            if (stripos($pageTitle, 'Goals Academy') !== false) {
+                                $totalsByDate[$date]['totalViews'] += $screenPageViews;
+                            }
+                        }
+                    } catch (Exception $e) {
+                        return response()->json(['error' => $e->getMessage()], 400);
+                    }
+
+                    return response()->json([
                         'status' => true,
                         'statusCode' => 200,
-                        'message' => 'get data category success',
                         'total_earning' => $totalEarning,
                         'total_order' => $totalOrder,
                         'total_checkout' => $totalCheckout,
                         'list_orders' => $order,
-                        "top_selling" => $topSellingProducts
+                        "top_selling" => $topSellingProducts,
+                        'totalClicksKeseluruhan' => $totalClicks,
+                        'totalViewsKeseluruhan' => $totalViews,
+                        'totalsByDate' => $totalsByDate,
                     ], 200);
                 }
             } else {
@@ -77,7 +216,8 @@ class OverviewController extends Controller
             return response()->json([
                 'status' => false,
                 'statusCode' => 500,
-                'message' => 'Internal Server Error'
+                'message' => 'Internal Server Error',
+                'error' => $e->getMessage()
             ], 500);
         }
     }
