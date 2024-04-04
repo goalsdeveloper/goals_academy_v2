358d83b5b49fec042fbbfeb2c30f39ad2c8dab81
Naufal Romero <88795011+romves@users.noreply.github.com>
2024-04-04 22:52:57 +0700

    fix(popup): fix mobile style

--- resources/js/Components/elements/GoalsPopup.jsx
+++ resources/js/Components/elements/GoalsPopup.jsx
@@ -25,7 +25,7 @@ const GoalsPopup = ({
                     show
                         ? "md:top-0 bottom-0 md:scale-100"
                         : "md:top-full -bottom-full md:scale-0"
-                } absolute mx-auto flex flex-col gap-[2vw] w-full md:w-[30vw] h-[50vh] md:h-fit transition-all duration-500 bg-white shadow-md rounded-t-[6vw] md:rounded-[1vw] p-[8vw] md:p-[1.75vw] z-50  md:mt-[8vh] inset-0 ${className}`}
+                } fixed md:absolute md:inset-0 mx-auto flex flex-col gap-[2vw] w-full md:w-[30vw] h-[50vh] md:h-fit transition-all duration-500 bg-white shadow-md rounded-t-[6vw] md:rounded-[1vw] p-[8vw] md:p-[1.75vw] z-50  md:mt-[8vh] ${className}`}
             >
                 {children}
             </div>
