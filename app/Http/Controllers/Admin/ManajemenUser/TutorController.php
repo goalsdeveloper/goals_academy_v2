71895ec2b6c5210001559e7a65bef1b0699c739c
afanokta <afanoktafianto99@gmail.com>
2024-05-08 20:21:58 +0700

    manajemen user controller

--- app/Http/Controllers/Admin/ManajemenUser/TutorController.php
+++ app/Http/Controllers/Admin/ManajemenUser/TutorController.php
@@ -2,14 +2,13 @@
 
 namespace App\Http\Controllers\Admin\ManajemenUser;
 
-use App\Models\User;
-use Inertia\Inertia;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Skill;
+use App\Models\User;
 use App\Models\UserProfile;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Inertia\Inertia;
 
 class TutorController extends Controller
 {
@@ -19,25 +18,26 @@ class TutorController extends Controller
     public function index(Request $request)
     {
         try {
-                // $search = $request->input('search');
-                // $perPage = $request->input('perPage', 10);
+            // $search = $request->input('search');
+            // $perPage = $request->input('perPage', 10);
 
-                $query = User::with('profile')->where("user_role", "tutor");
+            // if ($search) {
+            //     $query->where(function ($subquery) use ($search) {
+            //         $subquery->where('name', 'LIKE', "%$search%")
+            //             ->orWhere('username', 'LIKE', "%$search%");
+            //     });
+            // }
 
-                // if ($search) {
-                //     $query->where(function ($subquery) use ($search) {
-                //         $subquery->where('name', 'LIKE', "%$search%")
-                //             ->orWhere('username', 'LIKE', "%$search%");
-                //     });
-                // }
-                $tutors = $query->get();
-
-                return Inertia::render('Auth/Admin/ManajemenUser/Tutor', [
-                    'status' => true,
-                    'statusCode' => 200,
-                    'message' => 'get data success',
-                    'data' => $tutors,
-                ], 200);
+            return Inertia::render('Auth/Admin/ManajemenUser/Tutor', [
+                'status' => true,
+                'statusCode' => 200,
+                'message' => 'get data success',
+                'data' => function () {
+                    $query = User::with('profile')->where("user_role", "tutor");
+                    $tutors = $query->get();
+                    return $tutors;
+                },
+            ], 200);
         } catch (\Illuminate\Database\QueryException $e) {
             return response()->json(['status' => false, 'statusCode' => 500, 'message' => 'Failed to retrieve data. Internal Server Error'], 500);
         } catch (\Exception $e) {
@@ -45,16 +45,15 @@ class TutorController extends Controller
         }
     }
 
-
     /**
      * Show the form for creating a new resource.
      */
     public function create()
     {
-            $skill = Skill::get();
-            return response()->json(['status' => true, 'statusCode' => 200, 'data' => [
-                'skill' => $skill
-            ]], 200);
+        $skill = Skill::get();
+        return response()->json(['status' => true, 'statusCode' => 200, 'data' => [
+            'skill' => $skill,
+        ]], 200);
     }
 
     /**
@@ -63,22 +62,22 @@ class TutorController extends Controller
     public function store(Request $request)
     {
         try {
-                $validatedData = $request->validate([
-                    'name' => 'required|string',
-                    'email' => 'required|email|unique:users,email',
-                ]);
+            $validatedData = $request->validate([
+                'name' => 'required|string',
+                'email' => 'required|email|unique:users,email',
+            ]);
 
-                $user = User::create([
-                    'name' => $validatedData['name'],
-                    'username' => $validatedData['name'],
-                    'email' => $validatedData['email'],
-                    'password' => bcrypt('password'),
-                    'user_role' => 'tutor',
-                ]);
-                $userProfile = new UserProfile();
-                $userProfile->user_id = $user->id;
-                $userProfile->save();
-                return Inertia::location(route('admin.manajemen_user.tutor.index'));
+            $user = User::create([
+                'name' => $validatedData['name'],
+                'username' => $validatedData['name'],
+                'email' => $validatedData['email'],
+                'password' => bcrypt('password'),
+                'user_role' => 'tutor',
+            ]);
+            $userProfile = new UserProfile();
+            $userProfile->user_id = $user->id;
+            $userProfile->save();
+            return Inertia::location(route('admin.manajemen_user.tutor.index'));
 
         } catch (ValidationException $e) {
             return response()->json([
@@ -101,15 +100,15 @@ class TutorController extends Controller
     public function show(User $tutor)
     {
         try {
-                $tutorWithProfile = User::where("user_role", "tutor")->findOrFail($tutor->id);
-                $tutorWithProfile->load('profile', 'skills');
+            $tutorWithProfile = User::where("user_role", "tutor")->findOrFail($tutor->id);
+            $tutorWithProfile->load('profile', 'skills');
 
-                return Inertia::render('Auth/Admin/ManajemenUser/Tutor/Show', [
-                    'status' => true,
-                    'statusCode' => 200,
-                    'message' => 'get data success',
-                    'data' => $tutorWithProfile,
-                ], 200);
+            return Inertia::render('Auth/Admin/ManajemenUser/Tutor/Show', [
+                'status' => true,
+                'statusCode' => 200,
+                'message' => 'get data success',
+                'data' => $tutorWithProfile,
+            ], 200);
 
         } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
             return response()->json(['status' => false, 'statusCode' => 404, 'message' => 'User not found'], 404);
@@ -123,16 +122,16 @@ class TutorController extends Controller
      */
     public function edit(User $tutor)
     {
-            $skill = Skill::get();
-            $tutor->load('profile', 'skills');
-            return Inertia::render('Auth/Admin/ManajemenUser/Tutor/Update', [
-                'status' => true,
-                'statusCode' => 200,
-                'data' => [
-                    'tutor' => $tutor,
-                    'skill' => $skill
-                ]
-            ]);
+        $skill = Skill::get();
+        $tutor->load('profile', 'skills');
+        return Inertia::render('Auth/Admin/ManajemenUser/Tutor/Update', [
+            'status' => true,
+            'statusCode' => 200,
+            'data' => [
+                'tutor' => $tutor,
+                'skill' => $skill,
+            ],
+        ]);
 
     }
 
@@ -142,41 +141,40 @@ class TutorController extends Controller
     public function update(Request $request, $id)
     {
         try {
-                $tutor = User::findOrFail($id);
-                $validatedData = $request->validate([
-                    'name' => 'string',
-                    'username' => 'string',
-                    'phone_number' => 'string',
-                    'university' => 'string',
-                    'major' => 'string',
-                    'linkedin_url' => 'string',
-                    'skills' => 'array',
-                    'skills.*' => 'exists:skills,id',
-                    'profile_image' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
-                ]);
+            $tutor = User::findOrFail($id);
+            $validatedData = $request->validate([
+                'name' => 'string',
+                'username' => 'string',
+                'phone_number' => 'string',
+                'university' => 'string',
+                'major' => 'string',
+                'linkedin_url' => 'string',
+                'skills' => 'array',
+                'skills.*' => 'exists:skills,id',
+                'profile_image' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+            ]);
 
-                $tutor->update($validatedData);
+            $tutor->update($validatedData);
 
-                if ($request->has(['phone_number', 'university', 'major', 'linkedin_url'])) {
-                    $tutor->profile->update($validatedData);
+            if ($request->has(['phone_number', 'university', 'major', 'linkedin_url'])) {
+                $tutor->profile->update($validatedData);
+            }
+
+            if ($request->hasFile('profile_image')) {
+
+                if ($tutor->profile_image) {
+                    Storage::delete($tutor->profile_image);
                 }
 
+                $profileImagePath = $request->file('profile_image')->store('img/manajemen_user_tutor');
 
-                if ($request->hasFile('profile_image')) {
+                $tutor->profile->update(['profile_image' => $profileImagePath]);
+            }
 
-                    if ($tutor->profile_image) {
-                        Storage::delete($tutor->profile_image);
-                    }
+            $tutor->skills()->detach();
+            $tutor->skills()->attach($validatedData['skills']);
 
-                    $profileImagePath = $request->file('profile_image')->store('img/manajemen_user_tutor');
-
-                    $tutor->profile->update(['profile_image' => $profileImagePath]);
-                }
-
-                $tutor->skills()->detach();
-                $tutor->skills()->attach($validatedData['skills']);
-
-                return response()->json(['status' => true, 'statusCode' => 200, 'message' => 'update success'], 200);
+            return response()->json(['status' => true, 'statusCode' => 200, 'message' => 'update success'], 200);
 
         } catch (ValidationException $e) {
             return response()->json(['status' => false, 'statusCode' => 422, 'message' => $e->validator->errors()], 422);
@@ -190,5 +188,12 @@ class TutorController extends Controller
     public function destroy(User $tutor)
     {
         //
+    }
+
+    public function updateActive(Request $request, User $tutor)
+    {
+        // dd($request['is_active']);
+        $tutor->profile()->update(['is_active' => $request['is_active']]);
+        return redirect()->back();
     }
 }
