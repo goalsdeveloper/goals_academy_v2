9fc554d74f21ff20f91521d1304b645b722483a8
ilhamarkan2004 <ilhamarkan2004@gmail.com>
2024-05-10 17:14:02 +0700

    moderator

--- app/Http/Controllers/Moderator/Bimbingan/ModeratorHistoryBimbinganController.php
+++ app/Http/Controllers/Moderator/Bimbingan/ModeratorHistoryBimbinganController.php
@@ -86,9 +86,45 @@ class ModeratorHistoryBimbinganController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Course $history)
     {
-        //
+        try {
+            $tutors = User::with('profile')->where("user_role", "tutor")->get();
+            if (Auth::user()->user_role == "moderator") {
+                $history_user = Course::with('user:id,username', 'user.profile:id,user_id,university,major,phone_number,faculty', 'tutor:id,name', 'topic:id,topic', 'place.city', 'order:id,order_code', 'products:id,name', 'fileUploads', 'productReview')->findOrFail($history->id);
+                // return response()->json([
+                //     'status' => true,
+                //     'statusCode' => 200,
+                //     'message' => 'Get data success',
+                //     'data' => $history_user,
+                // ], 200);
+
+                return Inertia::render('Auth/Moderator/Bimbingan/Progress/View', [
+                    'progress' => $history_user,
+                    'tutors' => $tutors
+                ]);
+            } else {
+                abort(403);
+            }
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'status' => false,
+                'statusCode' => 404,
+                'message' => 'Course not found.',
+            ], 404);
+        } catch (AuthorizationException $e) {
+            return response()->json([
+                'status' => false,
+                'statusCode' => 403,
+                'message' => $e->getMessage(),
+            ], 403);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => false,
+                'statusCode' => 500,
+                'message' => 'An unexpected error occurred: ' . $e->getMessage(),
+            ], 500);
+        }
     }
 
     /**
