242d18be6d405acf852966f6f36758427f34411f
Akhmad Roziqin <roziqinakhmad14juli@gmail.com>
2024-05-30 17:12:47 +0700

    Tutor Dashboard : Responsive Bimbingan/Progress Update & Show, Bimbingan/History Show

--- resources/js/Pages/Auth/Admin/components/layouts/FormSection.jsx
+++ resources/js/Pages/Auth/Admin/components/layouts/FormSection.jsx
@@ -3,6 +3,8 @@ const FormSection = ({
     children = <></>,
     titleAction = <></>,
     className = "",
+    titleClassName,
+    wrapperClassName,
     bordered = false
 }) => {
     return (
@@ -10,11 +12,11 @@ const FormSection = ({
             className={`${bordered && "border"} bg-white w-full rounded-[.8vw] pt-[3.3vw] pb-[5.5vw] md:p-[3.3vw] space-y-[5.5vw] md:space-y-[1.6vw] ${className}`}
         >
             <div className="flex items-center justify-between">
-                <h2 className="text-[1vw] font-medium">{title}</h2>
+                <h2 className={`text-[1vw] font-medium ${titleClassName}`}>{title}</h2>
 
                 {titleAction}
             </div>
-            <div className="space-y-[1.2vw]">{children}</div>
+            <div className={`md:space-y-[1.2vw] ${wrapperClassName}`}>{children}</div>
         </div>
     );
 };
