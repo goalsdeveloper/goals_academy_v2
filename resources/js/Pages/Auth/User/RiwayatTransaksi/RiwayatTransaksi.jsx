90708eb11de6b0e1780dc5efcf076cfb0e2e73b5
Naufal Romero <88795011+romves@users.noreply.github.com>
2024-06-06 21:01:09 +0700

    style(user dash): refactor ui layout

--- resources/js/Pages/Auth/User/RiwayatTransaksi/RiwayatTransaksi.jsx
+++ resources/js/Pages/Auth/User/RiwayatTransaksi/RiwayatTransaksi.jsx
@@ -5,12 +5,18 @@ import { EmptyProductLayout } from "../Bimbingan/Bimbingan";
 import ProductListFilter from "../ProductListFilter";
 import { TransactionFilter } from "../constants";
 import RiwayatItem from "./components/RiwayatItem";
+import { useMediaQuery } from "react-responsive";
 
 export default function RiwayatTransaksi({ auth, dataOrder: data }) {
-console.log(data)
+    const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
+
     return (
         <UserLayout auth={auth} title="Riwayat Transaksi">
-            <div className="flex md:block justify-between items-center space-y-[1.2vw]">
+            <div
+                className={`${
+                    isMobile && "px-[7.4vw] w-full py-[1.2vw]"
+                } flex md:block justify-between items-center space-y-[1.2vw] `}
+            >
                 <h1 className="font-medium text-black text-[3.7vw] md:text-[1.8vw] leading-[12vw] md:leading-normal">
                     Riwayat Transaksi
                 </h1>
@@ -23,7 +29,7 @@ console.log(data)
                     redirectUrl="/produk"
                 />
             ) : (
-                <div className="md:min-h-[22vw] flex flex-col gap-[2vw] md:gap-[1vw]">
+                <div className="md:min-h-[22vw] flex flex-col md:gap-[1vw]">
                     {data.map((item, index) => {
                         return <RiwayatItem key={index} data={item} />;
                     })}
