9f33e979be1dc9482121c7ab0234447e69a94eb6
ilhamarkan2004 <ilhamarkan2004@gmail.com>
2024-06-27 20:50:07 +0700

    cronjob

---
+++ app/Console/Commands/ExpiredCourseCheckerCron.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Course;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class ExpiredCourseCheckerCron extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'expire:cron';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        info("Cron Job running at " . now());
+
+        /*------------------------------------------
+        --------------------------------------------
+        Write Your Logic Here....
+        I am getting users and create new users if not exist....
+        --------------------------------------------
+        --------------------------------------------*/
+        try {
+            //code...
+            // $response = Http::put('/api/check_expired');
+            $day_now = Carbon::now();
+            $courses = Course::whereNull('parent_id')->where("ongoing", "!=", "selesai")->get();
+            foreach ($courses as $course) {
+                $active_periode = $course->active_period;
+                $tanggal_beli = Carbon::parse($course->order->created_at)->addDays($active_periode);
+                if ($tanggal_beli < $day_now) {
+                    $course->is_moderator = true;
+                    if ($course->products->total_meet == "1"
+                    ) {
+                        $course->ongoing = "selesai";
+                    }
+                    $course->update();
+                    $course->child()->update(["is_moderator", true]);
+                };
+            }
+            Log::info('mantap');
+        } catch (\Throwable $th) {
+            Log::info($th->getMessage());
+        }
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\ExpiredCourseCheckerCron;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -10,9 +11,11 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      */
+    protected $commands = [ExpiredCourseCheckerCron::class];
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('expire:cron')->everyMinute();
     }
 
     /**
@@ -20,7 +23,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
