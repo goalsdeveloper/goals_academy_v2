8788b5ef9109a68866599f2606cfa324b09e395c
ilhamarkan2004 <ilhamarkan2004@gmail.com>
2024-07-11 16:02:18 +0700

    seeding skill tutor

--- database/seeders/SkillSeeder.php
+++ database/seeders/SkillSeeder.php
@@ -14,45 +14,75 @@ class SkillSeeder extends Seeder
     public function run(): void
     {
         // SOFTSKILL
-        Skill::create([
-            'name' => 'Kreatif',
-            'category' => SkillEnum::SOFTFKILL,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
-        Skill::create([
-            'name' => 'Komunikatif',
-            'category' => SkillEnum::SOFTFKILL,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
-        Skill::create([
-            'name' => 'Bertanggungjawab',
-            'category' => SkillEnum::SOFTFKILL,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
-        Skill::create([
-            'name' => 'Problem Solving',
-            'category' => SkillEnum::SOFTFKILL,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
-        Skill::create([
-            'name' => 'Inovatif',
-            'category' => SkillEnum::SOFTFKILL,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        // Skill::create([
+        //     'name' => 'Kreatif',
+        //     'category' => SkillEnum::SOFTFKILL,
+        //     'created_at' => now(),
+        //     'updated_at' => now(),
+        // ]);
+        // Skill::create([
+        //     'name' => 'Komunikatif',
+        //     'category' => SkillEnum::SOFTFKILL,
+        //     'created_at' => now(),
+        //     'updated_at' => now(),
+        // ]);
+        // Skill::create([
+        //     'name' => 'Bertanggungjawab',
+        //     'category' => SkillEnum::SOFTFKILL,
+        //     'created_at' => now(),
+        //     'updated_at' => now(),
+        // ]);
+        // Skill::create([
+        //     'name' => 'Problem Solving',
+        //     'category' => SkillEnum::SOFTFKILL,
+        //     'created_at' => now(),
+        //     'updated_at' => now(),
+        // ]);
+        // Skill::create([
+        //     'name' => 'Inovatif',
+        //     'category' => SkillEnum::SOFTFKILL,
+        //     'created_at' => now(),
+        //     'updated_at' => now(),
+        // ]);
         //HARDSKILL
         Skill::create([
-            'name' => 'Pemrograman',
+            'name' => 'Kuantitatif',
             'category' => SkillEnum::HARDSKILL,
             'created_at' => now(),
             'updated_at' => now(),
         ]);
         Skill::create([
-            'name' => 'SPSS',
+            'name' => 'Kualitatif',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Ekonomi Syariah',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Ilmu Komunikasi',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Manajemen',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Ekonomi',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Ilmu Hukum',
             'category' => SkillEnum::HARDSKILL,
             'created_at' => now(),
             'updated_at' => now(),
@@ -64,13 +94,313 @@ class SkillSeeder extends Seeder
             'updated_at' => now(),
         ]);
         Skill::create([
-            'name' => 'Kuantitatif',
+            'name' => 'Manajemen Sumber Daya Manusia',
             'category' => SkillEnum::HARDSKILL,
             'created_at' => now(),
             'updated_at' => now(),
         ]);
         Skill::create([
-            'name' => 'Kualitatif',
+            'name' => 'Manajemen Keuangan',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Pendidikan Geografi',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Teknik Sipil',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Ilmu Politik (Konsentrasi Kewirausahaan Politik dan Kebijakan)',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Teknik Informatika',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Evolusi dan Pemuliaan Tanaman Tropis dan Mediteranian',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Psikologi',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Sastra Inggris',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Hukum Tata Negara',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'SPSS',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Stata',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'EVIews',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Smart PLS',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Python',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'R Studio',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'amos',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Microsoft excel',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'VoS Viewer',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Perbankan',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Bisnis',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Manajemen Pemasaran',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Teknik Lingkungan',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Filsafat',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Administrasi Publik',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Ilmu Pendidikan dan Keguruan',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Google Earth',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'arcGIS',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Matematika',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Astronomi',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Biologi',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Pertanian',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Kehutanan',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Bahasa Prancis',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Ilmu Pendidikan',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => ' Sastra Indonesia',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Sastra Jepang ',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => ' Bimbingan Konseling',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Kebidanan',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Ilmu Politik',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Hubungan Internasional',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Administrasi Negara',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Skripsi',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Jurnal',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Proyek/Capstone',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Tesis',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Artikel',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Ilmiah',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Makalah',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Paper',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Working',
+            'category' => SkillEnum::HARDSKILL,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Skill::create([
+            'name' => 'Paper',
             'category' => SkillEnum::HARDSKILL,
             'created_at' => now(),
             'updated_at' => now(),
