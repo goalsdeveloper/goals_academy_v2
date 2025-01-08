4e67def2b900e94b068b41b3368442bbb508e958
roziqinakhmad14 <roziqinakhmad14juli@gmail.com>
2025-01-08 18:28:46 +0700

    refactor: remove import images code in SkripsiMastery landing page

--- resources/js/Pages/Partials/SkripsiMastery/PriceList.jsx
+++ resources/js/Pages/Partials/SkripsiMastery/PriceList.jsx
@@ -1,10 +1,6 @@
 import GoalsButton from "@/Components/elements/GoalsButton";
 import background from "/resources/img/vector/gradient-bg-8.svg";
 
-import img1 from "/resources/img/skripsi-mastery/skripsi-mastery-akses-1-bulan.svg";
-import img2 from "/resources/img/skripsi-mastery/skripsi-mastery-akses-3-bulan.svg";
-import img3 from "/resources/img/skripsi-mastery/skripsi-mastery-akses-6-bulan.svg";
-
 export default function PriceList ({ products }) {
     const data = [
         {
